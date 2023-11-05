c41c5dd16ffcbe8de376b4bba40415ab03f1610e
modjordan <mod.jordan@icloud.com>
2023-11-05 12:21:45 -0800

    Controller Function added.

---
+++ Migrations/20231105193734_SeedData2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CretaceousApi.Migrations
+{
+    public partial class SeedData2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1,
+                column: "Species",
+                value: "Woolly Mammoth");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1,
+                column: "Species",
+                value: "Wooly Mammoth");
+        }
+    }
+}
